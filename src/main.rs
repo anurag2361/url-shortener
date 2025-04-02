df15f2bec495a9e6388c6d94d9b8e12f58cb8e86
Anurag Sharma <anurag.sharma@leadschool.in>
2025-04-02 23:19:26 +0530

    added cors for frontend

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ mod structs;
 mod utils;
 
 use crate::state::app_state::AppState;
-use actix_web::{App, HttpServer, middleware::Logger, web};
+use actix_cors::Cors;
+use actix_web::{App, HttpServer, http, middleware::Logger, web};
 use db::mongodb::get_database;
 use dotenv::dotenv;
 use env_logger::Env;
@@ -37,8 +38,16 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         // Create a logger with a custom format instead
         let logger = Logger::new("%a \"%r\" %s %b \"%{Referer}i\" \"%{User-Agent}i\" %D ms");
+        // Enable CORS for all origins
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:5173") // Default Bun dev server port
+            .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
+            .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+            .allowed_header(http::header::CONTENT_TYPE)
+            .max_age(3600);
         App::new()
             .wrap(logger)
+            .wrap(cors)
             .app_data(app_state.clone())
             .configure(init_routes)
     })
